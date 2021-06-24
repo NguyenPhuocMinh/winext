4598ff281f37f026ff59500c1750f07a848dbe5b
Minh Nguyen <xiuquay55@gmail.com>
2021-06-24 15:01:03 +0700

    config error manager

--- utils/errors/base-error.js
+++ utils/errors/base-error.js
@@ -4,8 +4,6 @@ class BaseError extends Error {
   constructor(error) {
     super(error);
     this.name = 'WinNextBaseError';
-    this.message = error.message;
-    this.stack = error.stack;
   }
 }
 
