d9199d832597f0cf60fe9dd50161d6c8328e50c6
Minh Nguyen <xiuquay55@gmail.com>
2021-06-24 13:59:11 +0700

    modify for show error code

--- utils/errors/not-found-application-error.js
+++ utils/errors/not-found-application-error.js
@@ -6,6 +6,8 @@ class NotFoundApplicationError extends BaseError {
   constructor(error) {
     super(error);
     this.name = 'NotFoundApplicationError';
+    this.message = error.message;
+    this.stack = error.stack;
   }
 };
 
