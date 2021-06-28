4eb21a6b776d029ec295dd2953554aa6e561e5e8
Minh Nguyen <xiuquay55@gmail.com>
2021-06-28 11:37:54 +0700

    refactoring code

--- utils/errors/index.js
+++ utils/errors/index.js
@@ -2,8 +2,4 @@
 
 exports.BaseError = require('./base-error');
 
-exports.RequiredModuleError = require('./require-module-error');
 exports.NotFoundModuleError = require('./not-found-module-error');
-exports.NotFoundSandboxError = require('./not-found-sand-box-error');
-exports.NotFoundApplicationError = require('./not-found-application-error');
-exports.NotFoundDependencyError = require('./not-found-dependency-error');
