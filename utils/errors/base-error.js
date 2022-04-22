e9f28a8ef931350c84e3e29b30475913b1ddfbf3
Minh Nguyen <xiuquay55@gmail.com>
2022-04-23 00:50:51 +0700

    modify code

--- utils/errors/base-error.js
+++ utils/errors/base-error.js
@@ -4,6 +4,8 @@ class BaseError extends Error {
   constructor(error) {
     super(error);
     this.name = 'WinNextBaseError';
+    this.message = error.message;
+    this.stack = error.stack;
   }
 }
 
